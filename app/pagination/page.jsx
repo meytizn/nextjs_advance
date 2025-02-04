d351b958336ae97ee86a8d8c87e6b7bfe6262e24
meytizn <zamanianmehdi23desktop@gmail.com>
2025-02-04 15:15:12 +0330

    pagination successfully created with api

--- app/pagination/page.jsx
+++ app/pagination/page.jsx
@@ -2,23 +2,28 @@
 import Link from 'next/link'
 import React, { Fragment } from 'react'
 
-let products=[
-  {id:0 , title:"product 0 " , price:0} , 
-  {id:1 , title:"product 1 " , price:100} , 
-  {id:2 , title:"product 2 " , price:200} , 
-  {id:3 , title:"product 3 " , price:300} , 
-  {id:4 , title:"product 4 " , price:400} , 
-  {id:5 , title:"product 5 " , price:500} , 
-  {id:6 , title:"product 6 " , price:600} , 
-  {id:7 , title:"product 7 " , price:700} , 
-  {id:8 , title:"product 8 " , price:800} , 
-  {id:9 , title:"product 9 " , price:900}  
-]
+// let products=[
+//   {id:0 , title:"product 0 " , price:0} , 
+//   {id:1 , title:"product 1 " , price:100} , 
+//   {id:2 , title:"product 2 " , price:200} , 
+//   {id:3 , title:"product 3 " , price:300} , 
+//   {id:4 , title:"product 4 " , price:400} , 
+//   {id:5 , title:"product 5 " , price:500} , 
+//   {id:6 , title:"product 6 " , price:600} , 
+//   {id:7 , title:"product 7 " , price:700} , 
+//   {id:8 , title:"product 8 " , price:800} , 
+//   {id:9 , title:"product 9 " , price:900}  
+// ]
 
 
-export default function paginationpage({searchParams}) {
+export default async function paginationpage({searchParams}) {
   let ppg=3    //product page number 
   let page =parseInt(searchParams.page) || 1       //default current page  ?page=1
+
+
+  let response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  let products =await response.json()
+  let products_page_counter =Math.ceil( products.length / ppg )
 
   let firstIndex = (page-1) * ppg  //formula to get first index products
   let endIndex = firstIndex + ppg      //formula to get last index products
@@ -32,7 +37,7 @@ export default function paginationpage({searchParams}) {
       <ul >
           {showProducts.map((product , index) => (
           
-              <li className="m-auto" key={index}> {product.title} - $ {product.price} </li>
+              <li className="m-auto my-[50px] bg-green-300 py-[30px] rounded-md w-[95%] md:w-[50%]" key={index}> {product.title} <br/>{product.body} </li>
           
           ))}
         </ul>
@@ -44,8 +49,32 @@ export default function paginationpage({searchParams}) {
 
 
           <div className=' flex flex-row justify-around text-center w-[100%] py-1 my-5 text-white '>
-            <Link className=' bg-blue-500 w-[60px] h-[40px] rounded-md'  href={`?page=${page+1}`}> next </Link>
+
+
+
+
+            {page>1  && 
+            (
             <Link className=' bg-blue-500 w-[60px] h-[40px] rounded-md ' href={`?page=${page-1}`}> perv </Link>
+            )
+            
+            }
+
+
+
+            <div className='text-black'>{page} of {products_page_counter}</div>
+
+
+
+
+
+            {page<products_page_counter &&
+            (
+            <Link className=' bg-blue-500 w-[60px] h-[40px] rounded-md'  href={`?page=${page+1}`}> next </Link>
+            )
+            }
+
+
           </div>
 
     </Fragment>
