7daecfe176c25321b88c8f007b300cc7064f723d
meytizn <zamanianmehdi23desktop@gmail.com>
2025-02-06 17:15:20 +0330

    upgrading design

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className='bg-[#E2E8F0]'>{children}</body>
     </html>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -6,14 +6,21 @@ export default function App() {
   return (
     <Fragment>
       
-      < div className=" flex flex-col md:flex-row flex-wrap w-[100%]  m-auto text-[20px] text-center bg-blue-500 text-white gap-y-3">
+      <div className=" bg-[#E2E8F0] w-[100%] md:w-[100%] h-[80px] text-[30px] text-center m-auto border-b-4 border-indigo-500 ">
+        <div ><img className='w-[120px] m-auto'  src='next.png'/></div>
+        </div>
 
-        <div className="w-[100%] md:w-[25%] h-[60px] py-5"><Link href="/blog"><h1>SSG Rendering </h1></Link></div>
-        <div className="w-[100%] md:w-[25%] h-[60px] py-5"><Link href="/profile"><h1>SSR Rendering </h1></Link></div>
-        <div className="w-[100%] md:w-[25%] h-[60px] py-5"><Link href="/product"><h1>Components </h1></Link></div>
-        <div className="w-[100%] md:w-[25%] h-[60px] py-5"><h3><Link href="/api/pages/products"><h1>Api route </h1></Link></h3></div>
-        <div className="w-[100%] md:w-[25%] h-[60px] py-5"><h3><Link href="/suspense/"><h1>React suspense </h1></Link></h3></div>
-        <div className="w-[100%] md:w-[25%] h-[60px] py-5"><h3><Link href="/pagination/"><h1>Pagination </h1></Link></h3></div>
+
+
+      < div className=" flex flex-col md:flex-row flex-wrap w-[100%]  m-auto text-[20px] text-center  text-black gap-y-3">
+
+        <div className="w-[100%] md:w-[25%] h-[70px] py-5 hover:text-white hover:bg-indigo-500"><Link href="/blog"><h1>SSG Rendering </h1></Link></div>
+        <div className="w-[100%] md:w-[25%] h-[70px] py-5 hover:text-white hover:bg-indigo-500"><Link href="/profile"><h1>SSR Rendering </h1></Link></div>
+        <div className="w-[100%] md:w-[25%] h-[70px] py-5 hover:text-white hover:bg-indigo-500"><Link href="/product"><h1>Components </h1></Link></div>
+        <div className="w-[100%] md:w-[25%] h-[70px] py-5 hover:text-white hover:bg-indigo-500"><h3><Link href="/api/pages/products"><h1>Api route </h1></Link></h3></div>
+        <div className="w-[100%] md:w-[25%] h-[70px] py-5 hover:text-white hover:bg-indigo-500"><h3><Link href="/suspense/"><h1>React suspense </h1></Link></h3></div>
+        <div className="w-[100%] md:w-[25%] h-[70px] py-5 hover:text-white hover:bg-indigo-500"><h3><Link href="/pagination/"><h1>Pagination </h1></Link></h3></div>
+        <div className="w-[100%] md:w-[25%] h-[70px] py-5 hover:text-white hover:bg-indigo-500"><h3><Link href="/lazyloading/"><h1>Lazyloading </h1></Link></h3></div>
 
         </div> 
         
