e1ded10a0436e07dab75a9470bbe793f664e2a2e
meytizn <zamanianmehdi23desktop@gmail.com>
2025-02-06 17:23:47 +0330

    upgrade designing

--- app/page.jsx
+++ app/page.jsx
@@ -6,13 +6,13 @@ export default function App() {
   return (
     <Fragment>
       
-      <div className=" bg-[#E2E8F0] w-[100%] md:w-[100%] h-[80px] text-[30px] text-center m-auto border-b-4 border-indigo-500 ">
+      <div className=" bg-[#E2E8F0] w-[100%] md:w-[70%] h-[80px] text-[30px] text-center m-auto border-b-4 border-indigo-500  ">
         <div ><img className='w-[120px] m-auto'  src='next.png'/></div>
         </div>
 
 
 
-      < div className=" flex flex-col md:flex-row flex-wrap w-[100%]  m-auto text-[20px] text-center  text-black gap-y-3">
+      < div className=" flex flex-col md:flex-row flex-wrap w-[100%] md:w-[70%]  m-auto text-[20px] text-center  text-black gap-y-3 ">
 
         <div className="w-[100%] md:w-[25%] h-[70px] py-5 hover:text-white hover:bg-indigo-500"><Link href="/blog"><h1>SSG Rendering </h1></Link></div>
         <div className="w-[100%] md:w-[25%] h-[70px] py-5 hover:text-white hover:bg-indigo-500"><Link href="/profile"><h1>SSR Rendering </h1></Link></div>
