2f9658176ce381276826dcb27a065654fe8fce89
meytizn <zamanianmehdi23desktop@gmail.com>
2025-02-08 23:36:33 +0330

    ssg designed

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -3,14 +3,15 @@ import Link from 'next/link';
 async function Blog() {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
   let data = await response.json();
-  // data = data.slice(0, 50); // to use 10 posts
+   data = data.slice(0, 10); // to use 10 posts
+
   return (
     <>
-      <div className="my-[100px] m-auto">
+      <div className="w-[50%] m-auto">
         <ul>
           {data.map((item) => (
-            <Link key={item.id} href={`/blog/${item.id}`}>
-              <li className="m-auto">{item.id} - {item.title}</li>
+            <Link className='' key={item.id} href={`/blog/${item.id}`}>
+              <li className="text-indigo-600 py-5">{item.id} - {item.title}</li>
             </Link>
           ))}
         </ul>
